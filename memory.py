eec7b41e50e110de51a503ed00a77389977504fc
MD ALI SHEKH <mdalishekh2003@gmail.com>
2025-07-21 17:41:46 +0530

    Unchanged

--- memory.py
+++ memory.py
@@ -1,9 +1,9 @@
-# Memory efficiency is about to happen for this project
+# # Memory efficiency is about to happen for this project
 
-class MemoryCleaner:
-    def memory_management(self):
-        pass
+# class MemoryCleaner:
+#     def memory_management(self):
+#         pass
 
 
-    def cleanup_memory(self):
-        pass
+#     def cleanup_memory(self):
+#         pass
