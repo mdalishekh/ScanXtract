2dde1e868448b783ebdfacc59d314b62fa21b3a2
MD ALI SHEKH <mdalishekh2003@gmail.com>
2025-08-07 10:02:12 +0530

    Removed unnecessary lines of codes

--- memory.py
+++ memory.py
@@ -1,14 +1,6 @@
 # Memory efficiency is about to happen for this project
 
-class MemoryCleaner:
-    def memory_management(self):
-        pass
 
-
-    def cleanup_memory(self):
-        pass
-    
-    
 class MemoryOptimizer:
     def optimize_memory(self):
         pass
