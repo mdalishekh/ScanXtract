70ca2b761d815a34ef8992c5a5a333859945f514
MD ALI SHEKH <mdalishekh2003@gmail.com>
2025-08-07 10:03:43 +0530

    Added doc string in Memory optimizers

--- memory.py
+++ memory.py
@@ -3,8 +3,15 @@
 
 class MemoryOptimizer:
     def optimize_memory(self):
+        """
+        This method is intended to optimize memory usage in the application.
+        
+        """
         pass
 
     def reduce_memory_usage(self):
+        """
+        This method is intended to reduce memory usage in the application.
+        """
         pass    
     
